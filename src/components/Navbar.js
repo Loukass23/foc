c817d1d4cd711bcc8964cd9b2c1b4ca6a4e72036
loukass23 <l.dupias@net.estia.fr>
2020-05-21 17:47:21 +0200

    landing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
@@ -23,72 +22,67 @@ const Navbar = class extends React.Component {
         // set the class in state for the navbar accordingly
         this.state.active
           ? this.setState({
-              navBarActiveClass: 'is-active',
-            })
+            navBarActiveClass: 'is-active',
+          })
           : this.setState({
-              navBarActiveClass: '',
-            })
+            navBarActiveClass: '',
+          })
       }
     )
   }
 
   render() {
     return (
+
       <nav
         className="navbar is-transparent"
         role="navigation"
         aria-label="main-navigation"
+
       >
         <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-            </Link>
-            {/* Hamburger menu */}
-            <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
-            >
-              <span />
-              <span />
-              <span />
-            </div>
+          <div
+            className={`navbar-burger burger ${this.state.navBarActiveClass}`}
+            data-target="navMenu"
+            onClick={() => this.toggleHamburger()}
+          >
+            <span />
+            <span />
+            <span />
           </div>
+          {/* <div className="navbar-brand">
+            {/* <Link to="/" className="navbar-item" title="Logo">
+              <img src={logo} alt="Flightoclock" style={{ width: '88px' }} />
+            </Link> 
+
+
+        </div> */}
           <div
             id="navMenu"
             className={`navbar-menu ${this.state.navBarActiveClass}`}
+            style={{ display: 'flex', justifyContent: 'center' }}
           >
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
+            {/* <div className="navbar-end has-text-centered"> */}
+            <Link className="navbar-item" to="/about">
+              About
               </Link>
-              <Link className="navbar-item" to="/products">
-                Products
+            <Link className="navbar-item" to="/products">
+              Products
               </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
+            <Link className="navbar-item" to="/blog">
+              Blog
               </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
+            <Link className="navbar-item" to="/contact">
+              Contact
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
+            <Link className="navbar-item" to="/contact/examples">
+              Form Examples
               </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
-            </div>
           </div>
+          {/* <div className="navbar-end has-text-centered">
+           
+            </div> */}
+          {/* </div> */}
         </div>
       </nav>
     )
--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -4,49 +4,118 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content
-
+export const AboutPageTemplate = ({
+  image,
+  title,
+  heading,
+  subheading,
+  mainpitch,
+  description,
+  intro
+}) => {
   return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-              <PageContent className="content" content={content} />
-            </div>
+    <section className="hero is-fullheight-with-navbar"
+    >
+      <div className="hero-body"
+        style={{
+          display: 'flex',
+          justifyContent: 'center'
+        }}>
+
+        <div className="columns" style={{
+          height: '100%',
+          width: '100%'
+        }}>
+          <div className="column is-half"
+            style={{
+              height: '100%',
+              width: '100%',
+              backgroundImage: `url(${
+                !!image ? image.childImageSharp.fluid.src : image
+                })`,
+              backgroundPosition: `top center`,
+              backgroundAttachment: `fixed`,
+            }}>
+            First column
+          </div>
+          <div className="column is-half">
+            <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+              {title}
+            </h2>
           </div>
         </div>
+
       </div>
     </section>
+    /* <section className="section section--gradient"
+      style={{ position: 'absolute', width: '100vw', height: '100vh' }}>
+      <div className="container"
+      >
+        <div className="columns">
+          <div className="column"
+            style={{
+              height: '100',
+              backgroundImage: `url(${
+                !!image.childImageSharp ? image.childImageSharp.fluid.src : image
+                })`,
+              backgroundPosition: `top center`,
+              backgroundAttachment: `fixed`,
+            }}>
+            First column
+          </div>
+          <div className="column">
+            <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+              {title}
+            </h2>
+          </div>
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+
+              <PageContent className="content" content={content} />
+            </div> 
+        </div>
+        </div>
+      </div>
+    </section>*/
   )
 }
 
 AboutPageTemplate.propTypes = {
-  title: PropTypes.string.isRequired,
-  content: PropTypes.string,
-  contentComponent: PropTypes.func,
+  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  title: PropTypes.string,
+  heading: PropTypes.string,
+  subheading: PropTypes.string,
+  mainpitch: PropTypes.object,
+  description: PropTypes.string,
+  intro: PropTypes.shape({
+    blurbs: PropTypes.array,
+  }),
 }
 
 const AboutPage = ({ data }) => {
-  const { markdownRemark: post } = data
+  const { frontmatter } = data.markdownRemark
 
   return (
     <Layout>
       <AboutPageTemplate
-        contentComponent={HTMLContent}
-        title={post.frontmatter.title}
-        content={post.html}
+        image={frontmatter.image}
+        title={frontmatter.title}
+        heading={frontmatter.heading}
+        subheading={frontmatter.subheading}
+        mainpitch={frontmatter.mainpitch}
+        description={frontmatter.description}
+        intro={frontmatter.intro}
       />
     </Layout>
   )
 }
 
 AboutPage.propTypes = {
-  data: PropTypes.object.isRequired,
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
 }
 
 export default AboutPage
@@ -54,9 +123,36 @@ export default AboutPage
 export const aboutPageQuery = graphql`
   query AboutPage($id: String!) {
     markdownRemark(id: { eq: $id }) {
-      html
       frontmatter {
         title
+        image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        heading
+        subheading
+        mainpitch {
+          title
+          description
+        }
+        description
+        intro {
+          blurbs {
+            image {
+              childImageSharp {
+                fluid(maxWidth: 240, quality: 64) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            text
+          }
+          heading
+          description
+        }
       }
     }
   }
